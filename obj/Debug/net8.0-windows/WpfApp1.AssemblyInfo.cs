0d2d40c1f1f1261fcfbe2818e37a3c0b6b128490
JeremyKenton23 <jeremy.kenton@icloud.com>
2024-05-22 11:01:13 +0200

    Set up CI/CD pipeline with GitHub Actions

--- obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c93b8fd40f4429c67508943c5c92ec5aee0f141d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
