5965d57fa30d8912fca1cad790b903e06b9d16b9
JeremyKenton23 <jeremy.kenton@icloud.com>
2024-05-22 11:03:42 +0200

    Los merge conflicts op

--- obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c93b8fd40f4429c67508943c5c92ec5aee0f141d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7edf8e9b0f15d63f0b79c019c6d05ee8a0984d8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
